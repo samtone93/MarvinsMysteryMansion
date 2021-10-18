c94a5c60e8d5e63b20a30b43476e13b3629b9a69
changkev <38297595+kevinlichang@users.noreply.github.com>
2021-10-17 21:35:52 -0400

    set up simple go action verb function

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 # Marvin's Mystery Mansion
 
-import _json
+import json
 
+room = open("1.json")
+
+data = json.load(room)
+
+
+def go(curr_room, direction):
+    if curr_room["exits"][direction]:
+        return str(room["exits"][direction]) + ".json"
+
+
+print("Welcome")
+
+while True:
+
+    player_input = input(">")
+
+    if player_input == "go north":
+        room = open(go(data, "north"))
+        data = json.load(room)
+    elif player_input == "go east":
+        room = open(go(data, "east"))
+        data = json.load(room)
+    elif player_input == "go west":
+        room = open(go(data, "west"))
+        data = json.load(room)
+    elif player_input == "go south":
+        room = open(go(data, "south"))
+        data = json.load(room)
+    room.close()
+    print(data['roomName'])
