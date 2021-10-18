c780eddfb5fbe7c484a5bdbefeee1b994009adf6
changkev <38297595+kevinlichang@users.noreply.github.com>
2021-10-18 00:46:44 -0400

    fixed go function and adjusted if else statements

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ data = json.load(room)
 
 def go(curr_room, direction):
     if curr_room["exits"][direction]:
-        return str(room["exits"][direction]) + ".json"
+        return str(curr_room["exits"][direction]) + ".json"
 
 
 print("Welcome")
@@ -20,15 +20,13 @@ while True:
 
     if player_input == "go north":
         room = open(go(data, "north"))
-        data = json.load(room)
     elif player_input == "go east":
         room = open(go(data, "east"))
-        data = json.load(room)
     elif player_input == "go west":
         room = open(go(data, "west"))
-        data = json.load(room)
     elif player_input == "go south":
         room = open(go(data, "south"))
-        data = json.load(room)
+
+    data = json.load(room)
     room.close()
     print(data['roomName'])
