61b11a6f1faf41e6ac9db76bcee2f645f77dfade
Kenny Seng <sengk@oregonstate.edu>
2021-10-19 00:04:17 -0700

    Update main.py
    Game intro, short and long descriptions of rooms added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import json
 
-room = open("1.json")
+room = open("11.json")
 
 data = json.load(room)
 
@@ -11,11 +11,21 @@ def go(curr_room, direction):
     if curr_room["exits"][direction]:
         return str(curr_room["exits"][direction]) + ".json"
 
-
-print("Welcome")
+print()
+print("While you were going about your day, you were abducted and dropped off at an unknown location.")
+print("You feel the car stop and the driver leaves you with a letter before driving off in the distance.")
+print("It reads: “Hello <Player>, I hope this letter finds you well.")
+print("As my last remaining kin, I have left you the entirety of my fortune, which includes the mansion you currently stand at.")
+print("Inside, I hope you’ll find, decipher, and solve the clues I’ve left behind, to which will truly determine whether or not you are worthy of the Lehane family legacy.")
+print("Prevail, and you shall inherit it all; fail, and you will return to what you once were.")
+print()
 
 while True:
 
+    print("You enter the " + data['roomName'] + ".")
+    print(data['shortDesc'])
+    print(data['longDesc'])
+    
     player_input = input(">")
 
     if player_input == "go north":
@@ -29,4 +39,3 @@ while True:
 
     data = json.load(room)
     room.close()
-    print(data['roomName'])
